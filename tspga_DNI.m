7763428db09dfcd68c5369e2ac7c3e8eda61b418
Pablo Trelis <trelispablo@gmail.com>
2021-03-07 13:19:15 +0100

    Añadido menú gráfico con bugs

--- tspga_DNI.m
+++ tspga_DNI.m
@@ -1,4 +1,4 @@
-function tspga_DNI(fmodel,model)
+function tspga_DNI(fmode,model)
 % fucntion tspga_DNI(fmode,model)
 % Función que utiliza algoritmos genéticos para resolver el problema del
 % viajante (Travel Sales Problem TSP)
@@ -18,8 +18,16 @@ function tspga_DNI(fmodel,model)
 if nargin<1
     fmode=0;
 end
-numberofnodes=20;
-tam=10;
+but=0;
+global fig
+var=GeneraMenu();
+while(but ~=1) 
+    pause(1);
+end
+numberofnodes=var.numberofnodes.Value;
+tam=var.tam.Value;
+fmode=var.mode.Value;
+
 if fmode==3
     if nargin<2
          disp('Error: El modo 3 requiere introducir el modelo')
@@ -30,7 +38,7 @@ if fmode<3
 end
 x=model.x;
 y=model.y;
-numberofnodes=length(x);
+numberofnodes=length(x); 
 close all
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -40,9 +48,10 @@ npar=numberofnodes; % # of optimization variables
 Nt=npar; % # of columns in population matrix
 maxit=2000; % max number of iterations
 popsize=20; % set population size / miembros de la población
-mutrate=.05; % set mutation rate
+mutrate=0.05; % set mutation rate
 selection=0.5; % fraction of population kept / fracción de miembros sobreviven 
 
+mutnum=floor(mutrate*popsize);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %INICIALIZA LA POBLACIÓN Y VECTOR DE COSTES%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -50,103 +59,248 @@ selection=0.5; % fraction of population kept / fracción de miembros sobreviven
 individuo=zeros(popsize,npar);
 for i=1:1:popsize
     individuo(i,:)=randperm(npar);
-end 
-cost=zeros(1,popsize);
-for i=1:1:popsize
-    for j=1:1:npar
-        if j==npar
-            cost(i)=cost(i)+model.D(individuo(i,j),individuo(i,1));
-        else
-            cost(i)=cost(i)+model.D(individuo(i,j),individuo(i,j+1));
-        end
-    end
 end
+cost=tspfun(individuo,model);
+
 minc(1)=min(cost); % minc contains min of population
 meanc(1)=mean(cost); % meanc contains mean of population
-ordenada=zeros(popsize,npar);
-for i=1:1:popsize
-    [val,pose]=min(cost);
-    ordenada(i,:)=individuo(pose,:);
-    cost(pose)=inf;
-end
-pob=ordenada(1:popsize/2,:);
+% pop=zeros(popsize,npar); % pop es la matriz de población ordenada
+% dist=cost;
+% for i=1:1:popsize
+%     [val,pose]=min(cost);
+%     pop(i,:)=individuo(pose,:);
+%     %individuo(pose,:)=inf;
+%     cost(pose)=inf;   
+% end
+[cost,ind]=sort(cost); % Ordenamos vector costes
+pop=individuo(ind,:); % Población ordenada de mejor a peor
+N=ceil(popsize/2); % Calculo la mitad de la población
+pop=pop(1:N,:); % Eliminamos la mitad peor
+%sort(dist);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% GENERO EL VECTOR DE PROBABILIDADES %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% e.g. si tengo una población de 10 individuos y solo la mitad sobreviven
+% para reproducirse entonces el vector de probabilidades sería.
+% Prob=[5 4 4 3 3 3 2 2 2 2 1 1 1 1 1];
+Prob=repelem(N:-1:1, 1:N); % Genero vector probabilidades
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% PONEMOS EL MUNDO VIRTUAL A FUNCIONAR (MAIN LOOP) %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    iga=0;
+while iga<maxit
+    iga=iga+1; % increments generation counter  
+    pop=pop(1:N,:); % Eliminamos la mitad peor
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % ELIJO A LOS PADRES Y MADRES %
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % De entre el vector de probabilidades elijo aleatoriamente
+    % a los padres y a las madres.
+    for i=1:1:ceil(N/2)
+        padre{i}=pop(Prob(ceil(rand*length(Prob))),:);  
+        madre{i}=pop(Prob(ceil(rand*length(Prob))),:);       
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Emparejamientos y generación de los hijos %
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        [hijo1, hijo2]=HacerHijos(padre{i},madre{i});
+        
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Mutate the population
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        hijo1=Mutar(mutnum,hijo1);
+        hijo2=Mutar(mutnum,hijo2);
+        pop=[pop ;hijo1; hijo2]; % No pillamos hijos como padres, no actualizamos Prob
+    end
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Evalua el coste de la nueva población
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    cost=tspfun(pop,model);
+    % Sort the costs and associated parameters
+    [cost,ind]=sort(cost);
+    pop=pop(ind,:);
+
+    %_______________________________________________________
+    % Do statistics
+    minc(iga)=min(cost);
+    meanc(iga)=mean(cost);
+    
+    if mod(iga,100)==0 % Plot cada 100 interacciones
+        PlotViajero(model,100,pop(1,:));
+        pause(1/1e9) 
+    end
+    
+        
+    disp(iga)
+end %iga
+    PlotViajero(model,100,pop(1,:));
+    pause(1/1e9)
+    figure(2)
+    plot(minc);
+    hold on
+    plot(meanc);
+    hold off
+    disp(cost(1));
+    function [mutante]=Mutar(mutnum,vanilla)
+        r=zeros(mutnum,1);
+        x=zeros(mutnum,1);
+        mutante=vanilla;
+        for i=1:1:mutnum
+            r(i,1)=ceil(rand*length(vanilla));
+            r_prov=ceil(rand*length(vanilla));
+            while ismember(r_prov,r)
+                r_prov=ceil(rand*length(vanilla));
+            end
+            r(i,2)=r_prov;
+            x(i,1)=vanilla(r(i,1));
+            x(i,2)=vanilla(r(i,2));
+            vanilla(r(i,1))= x(i,2);
+            vanilla(r(i,2))= x(i,1);
+            mutante=vanilla;
+        end % end -> function Mutar
+    end
+    
+    function [cost]=tspfun(pop,model)
+        [popsize,npar]=size(pop);
+        cost=zeros(1,popsize);
+        for i=1:1:popsize
+            for j=1:1:npar
+                if j==npar
+                    cost(i)=cost(i)+model.D(pop(i,j),pop(i,1));
+                else
+                    cost(i)=cost(i)+model.D(pop(i,j),pop(i,j+1));
+                end
+            end
+        end
+    end % end -> function tspfun
+
+    function PlotViajero(model,tam,tabu)
+        clf 
+        rec=tabu;
+        hold on
+        for i=1:1:(length(tabu))
+            if i==length(tabu)
+            b_x=model.x(rec(1));
+            b_y=model.y(rec(1));
+            else
+            b_x=model.x(rec(i+1));
+            b_y=model.y(rec(i+1));
+            end
+            a_x=model.x(rec(i));
+            a_y=model.y(rec(i));
+            plot([a_x b_x],[a_y b_y],'b','LineWidth',1)
+        end
+        set(gca,'FontSize',12) %# Fix font size of the text in the current axes 
+        set(gca,'FontWeight','bold')  %# Fix Bold text in the current axes 
+        plot(model.x,model.y,'o') 
+        for i=1:length(model.dt.Points) %# Plot the number of each node
+            text(model.dt.Points(i,1),model.dt.Points(i,2),num2str(i),'FontWeight','bold')
+        end
+        axis([0 tam 0 tam]) %# Fix axes representation size
+        box on %# Plot all lines in the box of the figure
+        xlabel('X coordenate (m)') %# Label in the x axes
+        ylabel('Y coordenate (m)') %# Label in the y axes
+        title('Traveler') %# Title
+        hold off    
+    end % end -> function PlotViajero
+    
+    function [hijo1,hijo2]=HacerHijos(padre,madre)
+        hijos={0 0};
+        r=ceil(rand*length(padre));
+        for h=1:1:2
+            pp=[padre padre];        
+            genesp=pp(r:r+floor(length(padre)/2)-1);
+            genesm=madre;
+            for i=1:1:length(genesp)
+                genesm=genesm(genesm~=genesp(i));
+            end
+            hijo=[genesp genesm genesp genesm];
+            hijos{h}=hijo(length(padre)-r+2:length(padre)-r+1+length(padre));
+            x=padre;
+            padre=madre;
+            madre=x;
+        end
+        hijo1=hijos{1};
+        hijo2=hijos{2};
+        end % end -> function HacerHijos
 
     function [model]=generamodelo_DNI(numberofnodes,tam,mode);
-        close all % Se cierran los mapas abiertos, el menu no
-        %%%%%---------- Generacion inputs default ----------%%%%%
-        if nargin<3
-            mode=0; % Genera los nodos de forma aleatoria
-        end
-        if nargin<2
-            tam=100; % Tamaño del mapa 100x100 m
-        end
-        if nargin<1, 
-            numberofnodes=10; % 10 nodos deffault
-        end
-        %%%%%---------- Generacion de nodos segun el modo ----------%%%%%
-        if mode==0 % Se generan los nodos de forma aleatoria
-            if numberofnodes<3 % Para menos de 3 nodos, no es posible model
-                error('Seleccione un número de nodos válido mayor o igual que 3')
+            close all % Se cierran los mapas abiertos, el menu no
+            %%%%%---------- Generacion inputs default ----------%%%%%
+            if nargin<3
+                mode=0; % Genera los nodos de forma aleatoria
             end
-            x=tam*rand(numberofnodes,1); % Rand entre 0-1, multiplicamos por tam para
-            y=tam*rand(numberofnodes,1); % ajustarnos a la escala del mapa (tam x tam)
-        elseif mode==1 % Se solicita por pantalla las posiciones de los nodos
-            figure('Position',[800,150,600,500]);
-            axis([0 tam 0 tam]) % Se genera un plot vacío del tamaño tam
-            title({'\fontsize{10}Click izquierdo para seleccionar nodo,',...
-                'click derecho para seleccionar el último nodo y cerrar'})
-            [x,y]=getpts; % Se solicita y guarda en x,y las posiciones de los nodos
-            if numberofnodes ~=length(x) % Warning de reajuste de numero de nodos
-                warning('Se ha reajustado el número de nodos de: %d a: %d',...
-                    numberofnodes,length(x))
+            if nargin<2
+                tam=100; % Tamaño del mapa 100x100 m
             end
-            numberofnodes=length(x); % Recalculamos el valor del número de nodos
-            close all % Se cierra la figura de solicitud de nodos
-        elseif mode==2 % Se carga el modelo guardado en mapas.mat
-            load('mapas.mat');
-            numberofnodes=length(model.x);
-            x=model.x;
-            y=model.y;
-        else % Error si mode es diferente de 0,1 o 2
-            error('Error: Modo no válido, introduce un número entre 0,1 o 2') 
-        end % end -> if de modos
-        %%%%%---------- Generacion estructura delaunayTriangulation ----------%%%%%
-        dt=delaunayTriangulation(x,y);
-        % Con delaunayTriangulation generamos una estructura con:
-        % dt.Points: es una matriz numberofnodes x 2 que genera en cada fila las
-        % coord de cada nodo.
-        % dt.ConectivityList: matriz Nx3 donde N es el número de triángulos que
-        % tenemos, en cada fila encontramos los 3 nodos que representan los
-        % vértices de cada triángulo.
-        %%%%%---------- Calculo matriz distancias entre nodos D ----------%%%%%
-        for i = 1:1:numberofnodes
-            for j = 1:1:numberofnodes
-                D(i,j)= sqrt((dt.Points(i,1)-dt.Points(j,1))^2+(dt.Points(i,2)-dt.Points(j,2))^2);
-            end 
-        end
-        % Se recorre una matriz generando la distancia entre los diferentes 
-        % nodos, donde D(1,2) es la distancia entre los nodos 1 y 2.
-        %%%%%---------- Calculo nodos vecinos ----------%%%%%
-        for i = 1:1:numberofnodes % Se recorren los x nodos comprobando si estan conectados
-            for j = 1:1:numberofnodes
-                neiM(i,j)=isConnected(dt,i,j);
-            % Matriz de valores lógicos que indica si dos nodos estan conec
-            % o no, neiM(1,2)=0 indica que los nodos 1 y 2 no son vecinos.
-            % neiM(1,2)=1 indica que los nodos 1 y 2 son vecinos.
+            if nargin<1, 
+                numberofnodes=10; % 10 nodos deffault
             end
-            nei{i}=find(neiM(i,:)); % Se genera el vector con los vecinos de cada nodo
-            clear neiM % No se necesita la matriz binaria
-        end
-        %%%%%---------- Generacion outputs struct model. ----------%%%%%
-        model.dt=dt; % Struct dt contiene estructura triangulacion
-        model.x=x; % Vector de posiciones x de nodos
-        model.y=y; % Vector posiciones y de nodos
-        model.D=D; % Matriz de distancias entre nodos
-        model.nei=nei; % Vector de vecinos de cada nodo
-        %%%%%---------- Plot output ----------%%%%%
-        MapaPlot(model,tam)   
+            %%%%%---------- Generacion de nodos segun el modo ----------%%%%%
+            if mode==0 % Se generan los nodos de forma aleatoria
+                if numberofnodes<3 % Para menos de 3 nodos, no es posible model
+                    error('Seleccione un número de nodos válido mayor o igual que 3')
+                end
+                x=tam*rand(numberofnodes,1); % Rand entre 0-1, multiplicamos por tam para
+                y=tam*rand(numberofnodes,1); % ajustarnos a la escala del mapa (tam x tam)
+            elseif mode==1 % Se solicita por pantalla las posiciones de los nodos
+                figure('Position',[800,150,600,500]);
+                axis([0 tam 0 tam]) % Se genera un plot vacío del tamaño tam
+                title({'\fontsize{10}Click izquierdo para seleccionar nodo,',...
+                    'click derecho para seleccionar el último nodo y cerrar'})
+                [x,y]=getpts; % Se solicita y guarda en x,y las posiciones de los nodos
+                if numberofnodes ~=length(x) % Warning de reajuste de numero de nodos
+                    warning('Se ha reajustado el número de nodos de: %d a: %d',...
+                        numberofnodes,length(x))
+                end
+                numberofnodes=length(x); % Recalculamos el valor del número de nodos
+                close all % Se cierra la figura de solicitud de nodos
+            elseif mode==2 % Se carga el modelo guardado en mapas.mat
+                load('mapas.mat');
+                numberofnodes=length(model.x);
+                x=model.x;
+                y=model.y;
+            else % Error si mode es diferente de 0,1 o 2
+                error('Error: Modo no válido, introduce un número entre 0,1 o 2') 
+            end % end -> if de modos
+            %%%%%---------- Generacion estructura delaunayTriangulation ----------%%%%%
+            dt=delaunayTriangulation(x,y);
+            % Con delaunayTriangulation generamos una estructura con:
+            % dt.Points: es una matriz numberofnodes x 2 que genera en cada fila las
+            % coord de cada nodo.
+            % dt.ConectivityList: matriz Nx3 donde N es el número de triángulos que
+            % tenemos, en cada fila encontramos los 3 nodos que representan los
+            % vértices de cada triángulo.
+            %%%%%---------- Calculo matriz distancias entre nodos D ----------%%%%%
+            for i = 1:1:numberofnodes
+                for j = 1:1:numberofnodes
+                    D(i,j)= sqrt((dt.Points(i,1)-dt.Points(j,1))^2+(dt.Points(i,2)-dt.Points(j,2))^2);
+                end 
+            end
+            % Se recorre una matriz generando la distancia entre los diferentes 
+            % nodos, donde D(1,2) es la distancia entre los nodos 1 y 2.
+            %%%%%---------- Calculo nodos vecinos ----------%%%%%
+            for i = 1:1:numberofnodes % Se recorren los x nodos comprobando si estan conectados
+                for j = 1:1:numberofnodes
+                    neiM(i,j)=isConnected(dt,i,j);
+                % Matriz de valores lógicos que indica si dos nodos estan conec
+                % o no, neiM(1,2)=0 indica que los nodos 1 y 2 no son vecinos.
+                % neiM(1,2)=1 indica que los nodos 1 y 2 son vecinos.
+                end
+                nei{i}=find(neiM(i,:)); % Se genera el vector con los vecinos de cada nodo
+                clear neiM % No se necesita la matriz binaria
+            end
+            %%%%%---------- Generacion outputs struct model. ----------%%%%%
+            model.dt=dt; % Struct dt contiene estructura triangulacion
+            model.x=x; % Vector de posiciones x de nodos
+            model.y=y; % Vector posiciones y de nodos
+            model.D=D; % Matriz de distancias entre nodos
+            model.nei=nei; % Vector de vecinos de cada nodo
+            %%%%%---------- Plot output ----------%%%%%
+            MapaPlot(model,tam)   
     end % end -> function generamodelo_DNI
+        
     function []=MapaPlot(model,tam) % Funcion crea el plot del mapa 
         figure('Position',[800,150,600,500]); % Posición y tamaño de fig
         set(gca,'FontSize',12) %# Fix font size of the text in the current axes 
@@ -162,4 +316,55 @@ pob=ordenada(1:popsize/2,:);
         ylabel('Y coordenate (m)') %# Label in the y axes
         title('Generated model') %# Title
     end % end -> function MapaPlot
+
+    function [var]=GeneraMenu()
+    % La función GeneraMenu crea todos los elementos del uifigure que se
+    % utilizan como menú para las diferentes aplicaciones. Se generan
+    % diferentes label que indican que es cada elemento. Se utilizan
+    % tambien cajas de números para añadir el valor deseado a las variables
+    % y botones para ejecutar las correspondientes funciones.        
+    %%%%%---------- Creacion uifigure principal ----------%%%%%
+    fig = uifigure; %Se crea la ui figure principal y su posicion
+    fig.Position = [50,100,700,160];
+    fig.HandleVisibility = 'on';
+    %%%%%---------- Creacion paneles para cada app ----------%%%%%
+    p1 = uipanel('Parent',fig,'Position',[10,10,680,140]); %Panel Mapa
+    %%%%%---------- Diferentes labels del menu ----------%%%%%
+    % Submenu Generacion de mapa panel 1 Labels
+    sec = uilabel('Parent',p1,'Position',[250,100, 200,30],'HorizontalAlignment','center');
+    sec.FontSize = 18;
+    sec.Text = 'Generación de Mapa';
+    inf = uilabel('Parent',p1,'Position',[30,70, 100,20]);
+    inf.Text = 'Número de nodos';
+    inf = uilabel('Parent',p1,'Position',[180,70, 100,20]);
+    inf.Text = 'Tamaño tabla';
+    inf = uilabel('Parent',p1,'Position',[330,70, 100,20]);
+    inf.Text = 'Modo';
+    %%%%%---------- Elementos solicitud de variables ----------%%%%%
+    %--- Info: Las variables solicitadas se guardan en el struct var.
+    % Variables generacion de mapa: numberofnodes, tam y mode
+    var.numberofnodes = uieditfield(p1,'numeric','Position',[30,50, 100,20]);
+    var.numberofnodes.Value = 20;
+    var.tam = uieditfield(p1,'numeric','Position',[180,50, 100,20]);
+    var.tam.Value = 100;
+    var.mode = uidropdown(p1,'Position',[330,50, 100,20]);
+    var.mode.Items = {'Aleatorio','Seleccionar','Guardado','Variable Model'};
+    var.mode.ItemsData = [0 1 2 3];
+    var.mode.Value=fmode;
+    %%%%%---------- Botones del menu ----------%%%%%
+    % Botones generacion de mapa
+    btn = uibutton(p1,'push','Text','Generar mapa',...
+               'Position',[480, 50, 100, 20],...
+               'ButtonPushedFcn', @(btn,event)btnPush());
+           
+    %%%%%---------- Funciones btnPush ----------%%%%%
+        function btnPush()
+            but=1;
+        end
+        
+        
+    end % end -> function GeneraMenu
+
+
+
 end % end -> function tspga_DNI
